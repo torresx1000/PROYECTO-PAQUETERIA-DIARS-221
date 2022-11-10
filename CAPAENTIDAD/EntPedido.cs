cb58ebd4a7a0592e74f8578beb99253c4aa991be
yisus0607 <bernuijesus363@gmail.com>
2022-11-09 21:03:28 -0500

    bd entidad

--- CAPAENTIDAD/EntPedido.cs
+++ CAPAENTIDAD/EntPedido.cs
@@ -8,29 +8,29 @@ namespace CAPAENTIDAD
 {
     public class EntPedido
     {
-        public int  IdPedido { get; set; }
+        public string  Encargado { get; set; }
         public int DniR { get; set; }
+        public string NombreR { get; set; }
         public string CorreoR { get; set; } 
         public int TelefonoR { get; set; }
         public string DireccionR    { get; set; }
-        public string NombreR { get; set; }
-        public string ObservacionesR  { get; set; }
-        public int ContactoR { get; set; }
         public string DistritoR { get; set; }
         public string ProvinciaR { get; set; }
         public string DepartamentoR { get; set; }
 
         public int DniD { get; set; }
+        public string NombreD { get; set; }
         public string CorreoD { get; set; }
         public int TelefonoD { get; set; }
         public string DireccionD { get; set; }
-        public string NombreD { get; set; }
-        public string ObservacionesD { get; set; }
-        public int ContactoD { get; set; }
         public string DistritoD { get; set; }
         public string ProvinciaD { get; set; }
         public string DepartamentoD { get; set; }
 
+        public int CodigoPaquete { get; set; }
+        public int CantidadPaquete { get; set; }
+        public string Categoria { get; set; }
+        public string Descripcion { get; set; }
 
     }
 }
