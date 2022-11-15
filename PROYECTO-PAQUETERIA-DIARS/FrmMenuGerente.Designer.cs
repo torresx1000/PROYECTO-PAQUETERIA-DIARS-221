c784603a96bd14c2088af9305757cd3a36ba21ec
yisus0607 <bernuijesus363@gmail.com>
2022-11-14 23:53:10 -0500

    se you later

--- PROYECTO-PAQUETERIA-DIARS/FrmMenuGerente.Designer.cs
+++ PROYECTO-PAQUETERIA-DIARS/FrmMenuGerente.Designer.cs
@@ -29,14 +29,14 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.materialRaisedButton3 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialRaisedButton2 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialRaisedButton1 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.btnAtrabajadores = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.btnAdminLogin = new MaterialSkin.Controls.MaterialRaisedButton();
             this.panel2 = new System.Windows.Forms.Panel();
             this.lblNombre = new System.Windows.Forms.Label();
             this.lblCargo = new System.Windows.Forms.Label();
-            this.btnAdminLogin = new MaterialSkin.Controls.MaterialRaisedButton();
-            this.btnAtrabajadores = new MaterialSkin.Controls.MaterialRaisedButton();
-            this.materialRaisedButton1 = new MaterialSkin.Controls.MaterialRaisedButton();
-            this.materialRaisedButton2 = new MaterialSkin.Controls.MaterialRaisedButton();
-            this.materialRaisedButton3 = new MaterialSkin.Controls.MaterialRaisedButton();
             this.panelContenedor = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -53,106 +53,111 @@
             this.panel1.Controls.Add(this.btnAtrabajadores);
             this.panel1.Controls.Add(this.btnAdminLogin);
             this.panel1.Controls.Add(this.panel2);
-            this.panel1.Location = new System.Drawing.Point(1, 66);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Location = new System.Drawing.Point(1, 81);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(222, 555);
+            this.panel1.Size = new System.Drawing.Size(296, 683);
             this.panel1.TabIndex = 0;
+            // 
+            // materialRaisedButton3
+            // 
+            this.materialRaisedButton3.Depth = 0;
+            this.materialRaisedButton3.Location = new System.Drawing.Point(171, 640);
+            this.materialRaisedButton3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.materialRaisedButton3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton3.Name = "materialRaisedButton3";
+            this.materialRaisedButton3.Primary = true;
+            this.materialRaisedButton3.Size = new System.Drawing.Size(100, 28);
+            this.materialRaisedButton3.TabIndex = 0;
+            this.materialRaisedButton3.Text = "SALIR";
+            this.materialRaisedButton3.UseVisualStyleBackColor = true;
+            this.materialRaisedButton3.Click += new System.EventHandler(this.materialRaisedButton3_Click);
+            // 
+            // materialRaisedButton2
+            // 
+            this.materialRaisedButton2.Depth = 0;
+            this.materialRaisedButton2.Location = new System.Drawing.Point(25, 551);
+            this.materialRaisedButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.materialRaisedButton2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton2.Name = "materialRaisedButton2";
+            this.materialRaisedButton2.Primary = true;
+            this.materialRaisedButton2.Size = new System.Drawing.Size(247, 52);
+            this.materialRaisedButton2.TabIndex = 0;
+            this.materialRaisedButton2.Text = "REGISTRAR ORDEN DE COMPRA";
+            this.materialRaisedButton2.UseVisualStyleBackColor = true;
+            this.materialRaisedButton2.Click += new System.EventHandler(this.materialRaisedButton2_Click);
+            // 
+            // materialRaisedButton1
+            // 
+            this.materialRaisedButton1.Depth = 0;
+            this.materialRaisedButton1.Location = new System.Drawing.Point(25, 457);
+            this.materialRaisedButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.materialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton1.Name = "materialRaisedButton1";
+            this.materialRaisedButton1.Primary = true;
+            this.materialRaisedButton1.Size = new System.Drawing.Size(247, 52);
+            this.materialRaisedButton1.TabIndex = 0;
+            this.materialRaisedButton1.Text = "ADMINISTRAR VEHICULO";
+            this.materialRaisedButton1.UseVisualStyleBackColor = true;
+            this.materialRaisedButton1.Click += new System.EventHandler(this.materialRaisedButton1_Click_1);
+            // 
+            // btnAtrabajadores
+            // 
+            this.btnAtrabajadores.Depth = 0;
+            this.btnAtrabajadores.Location = new System.Drawing.Point(25, 364);
+            this.btnAtrabajadores.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAtrabajadores.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnAtrabajadores.Name = "btnAtrabajadores";
+            this.btnAtrabajadores.Primary = true;
+            this.btnAtrabajadores.Size = new System.Drawing.Size(248, 52);
+            this.btnAtrabajadores.TabIndex = 3;
+            this.btnAtrabajadores.Text = "ADMINISTRAR TRABAJADORES";
+            this.btnAtrabajadores.UseVisualStyleBackColor = true;
+            this.btnAtrabajadores.Click += new System.EventHandler(this.materialRaisedButton1_Click);
+            // 
+            // btnAdminLogin
+            // 
+            this.btnAdminLogin.Depth = 0;
+            this.btnAdminLogin.Location = new System.Drawing.Point(24, 250);
+            this.btnAdminLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAdminLogin.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnAdminLogin.Name = "btnAdminLogin";
+            this.btnAdminLogin.Primary = true;
+            this.btnAdminLogin.Size = new System.Drawing.Size(248, 52);
+            this.btnAdminLogin.TabIndex = 0;
+            this.btnAdminLogin.Text = "ADMINISTRAR LOGIN";
+            this.btnAdminLogin.UseVisualStyleBackColor = true;
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.panel2.Controls.Add(this.lblNombre);
             this.panel2.Controls.Add(this.lblCargo);
-            this.panel2.Location = new System.Drawing.Point(2, 2);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Location = new System.Drawing.Point(3, 2);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(213, 183);
+            this.panel2.Size = new System.Drawing.Size(284, 225);
             this.panel2.TabIndex = 1;
             // 
             // lblNombre
             // 
             this.lblNombre.AutoSize = true;
-            this.lblNombre.Location = new System.Drawing.Point(89, 132);
-            this.lblNombre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(77, 160);
             this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(35, 13);
+            this.lblNombre.Size = new System.Drawing.Size(104, 25);
             this.lblNombre.TabIndex = 0;
-            this.lblNombre.Text = "label2";
+            this.lblNombre.Text = "NOMBRE";
             // 
             // lblCargo
             // 
             this.lblCargo.AutoSize = true;
-            this.lblCargo.Location = new System.Drawing.Point(89, 155);
-            this.lblCargo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCargo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCargo.Location = new System.Drawing.Point(37, 185);
             this.lblCargo.Name = "lblCargo";
-            this.lblCargo.Size = new System.Drawing.Size(35, 13);
+            this.lblCargo.Size = new System.Drawing.Size(90, 25);
             this.lblCargo.TabIndex = 0;
-            this.lblCargo.Text = "label1";
-            // 
-            // btnAdminLogin
-            // 
-            this.btnAdminLogin.Depth = 0;
-            this.btnAdminLogin.Location = new System.Drawing.Point(18, 203);
-            this.btnAdminLogin.MouseState = MaterialSkin.MouseState.HOVER;
-            this.btnAdminLogin.Name = "btnAdminLogin";
-            this.btnAdminLogin.Primary = true;
-            this.btnAdminLogin.Size = new System.Drawing.Size(186, 42);
-            this.btnAdminLogin.TabIndex = 0;
-            this.btnAdminLogin.Text = "ADMINISTRAR LOGIN";
-            this.btnAdminLogin.UseVisualStyleBackColor = true;
-            // 
-            // btnAtrabajadores
-            // 
-            this.btnAtrabajadores.Depth = 0;
-            this.btnAtrabajadores.Location = new System.Drawing.Point(19, 296);
-            this.btnAtrabajadores.MouseState = MaterialSkin.MouseState.HOVER;
-            this.btnAtrabajadores.Name = "btnAtrabajadores";
-            this.btnAtrabajadores.Primary = true;
-            this.btnAtrabajadores.Size = new System.Drawing.Size(186, 42);
-            this.btnAtrabajadores.TabIndex = 3;
-            this.btnAtrabajadores.Text = "ADMINISTRAR TRABAJADORES";
-            this.btnAtrabajadores.UseVisualStyleBackColor = true;
-            this.btnAtrabajadores.Click += new System.EventHandler(this.materialRaisedButton1_Click);
-            // 
-            // materialRaisedButton1
-            // 
-            this.materialRaisedButton1.Depth = 0;
-            this.materialRaisedButton1.Location = new System.Drawing.Point(19, 371);
-            this.materialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER;
-            this.materialRaisedButton1.Name = "materialRaisedButton1";
-            this.materialRaisedButton1.Primary = true;
-            this.materialRaisedButton1.Size = new System.Drawing.Size(185, 42);
-            this.materialRaisedButton1.TabIndex = 0;
-            this.materialRaisedButton1.Text = "ADMINISTRAR VEHICULO";
-            this.materialRaisedButton1.UseVisualStyleBackColor = true;
-            this.materialRaisedButton1.Click += new System.EventHandler(this.materialRaisedButton1_Click_1);
-            // 
-            // materialRaisedButton2
-            // 
-            this.materialRaisedButton2.Depth = 0;
-            this.materialRaisedButton2.Location = new System.Drawing.Point(19, 448);
-            this.materialRaisedButton2.MouseState = MaterialSkin.MouseState.HOVER;
-            this.materialRaisedButton2.Name = "materialRaisedButton2";
-            this.materialRaisedButton2.Primary = true;
-            this.materialRaisedButton2.Size = new System.Drawing.Size(185, 42);
-            this.materialRaisedButton2.TabIndex = 0;
-            this.materialRaisedButton2.Text = "REGISTRAR ORDEN DE COMPRA";
-            this.materialRaisedButton2.UseVisualStyleBackColor = true;
-            this.materialRaisedButton2.Click += new System.EventHandler(this.materialRaisedButton2_Click);
-            // 
-            // materialRaisedButton3
-            // 
-            this.materialRaisedButton3.Depth = 0;
-            this.materialRaisedButton3.Location = new System.Drawing.Point(128, 520);
-            this.materialRaisedButton3.MouseState = MaterialSkin.MouseState.HOVER;
-            this.materialRaisedButton3.Name = "materialRaisedButton3";
-            this.materialRaisedButton3.Primary = true;
-            this.materialRaisedButton3.Size = new System.Drawing.Size(75, 23);
-            this.materialRaisedButton3.TabIndex = 0;
-            this.materialRaisedButton3.Text = "SALIR";
-            this.materialRaisedButton3.UseVisualStyleBackColor = true;
-            this.materialRaisedButton3.Click += new System.EventHandler(this.materialRaisedButton3_Click);
+            this.lblCargo.Text = "CARGO";
             // 
             // panelContenedor
             // 
@@ -161,21 +166,21 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panelContenedor.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.panelContenedor.BackgroundImage = global::PROYECTO_PAQUETERIA_DIARS.Properties.Resources.WhatsApp_Image_2022_11_12_at_5_59_25_PM__1_;
-            this.panelContenedor.Location = new System.Drawing.Point(227, 69);
-            this.panelContenedor.Margin = new System.Windows.Forms.Padding(2);
+            this.panelContenedor.Location = new System.Drawing.Point(303, 85);
+            this.panelContenedor.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panelContenedor.Name = "panelContenedor";
-            this.panelContenedor.Size = new System.Drawing.Size(708, 552);
+            this.panelContenedor.Size = new System.Drawing.Size(944, 679);
             this.panelContenedor.TabIndex = 1;
             this.panelContenedor.Paint += new System.Windows.Forms.PaintEventHandler(this.panelContenedor_Paint);
             // 
             // FrmMenuGerente
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(946, 632);
+            this.ClientSize = new System.Drawing.Size(1261, 778);
             this.Controls.Add(this.panelContenedor);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FrmMenuGerente";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormMenuGerente";
